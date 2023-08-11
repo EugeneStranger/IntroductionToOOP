21a6d91713a011e82fad218922f25979d0cf770a
admin <admin@H>
2023-08-11 12:38:15 +0300

    Fraction Done

--- Fraction/Main.cpp
+++ Fraction/Main.cpp
@@ -5,6 +5,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#define delimiter "\n----------------------------------------------------\n"
+
 class Fraction;
 Fraction operator*(Fraction left, Fraction right);
 Fraction operator/(                                                                                                                                           const Fraction left, const Fraction right);
@@ -51,12 +53,23 @@ public:
 		this->denominator = 1;
 		cout << "Default Constructor:\t" << this << endl;
 	}
-	Fraction(int integer)
+	explicit Fraction(int integer)
 	{
 		this->integer = integer;
 		this->numerator = 0;
 		this->denominator = 1;
 		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	Fraction(double decimal)
+	{
+		decimal += 1e-10;// 0.0000000001 (1*10^(-10))
+		integer = decimal;
+		decimal -= integer;
+		denominator = 1e+9; //1*10^(9), 10 в 9-й степени, точность всегда будет 9 знаков после запятой
+		numerator = decimal * denominator;
+		reduce();
+		cout << "1ArgDConstructor:\t" << this << endl;
+
 	}
 	Fraction(int numerator, int denominator)
 	{
@@ -89,7 +102,7 @@ public:
 		this->integer = other.integer;
 		this->numerator = other.numerator;
 		this->denominator = other.denominator;
-		cout << "CopyAssignment:\t" << this << endl;
+		cout << "CopyAssignment:\t\t" << this << endl;
 		return *this;
 	}
 	Fraction& operator*=(const Fraction& other)
@@ -108,7 +121,6 @@ public:
 	{
 		return *this = *this - other;
 	}
-
 	Fraction& operator++() //Prefix increment
 	{
 		this->integer++;
@@ -131,6 +143,17 @@ public:
 		integer--;
 		return old;
 	}
+
+	//Type-cast operators
+	explicit operator int() const
+	{
+		return integer;
+	}
+	explicit operator double() const
+	{
+		return integer + (double)numerator/denominator;
+	}
+
 	// Methods
 	void print()const
 	{
@@ -300,6 +323,9 @@ std::istream& operator>>(std::istream& is, Fraction& obj)
 //#define CONSTRUCTORS_CHECK
 //#define INPUT_CHECK1
 //#define INPUT_CHECK2
+//#define TYPE_CONVERSIONS
+//#define CONVERSIONS_FROM_OTHER_TO_CLASS
+//#define CONVERSIONS_FROM_CLASS_TO_OTHER
 
 
 void main()
@@ -324,7 +350,7 @@ void main()
 	Fraction F;				//CopyAssignment
 	F = D;
 	F.print();
-#endif;
+#endif;ghvh
 #ifdef INPUT_CHECK1
 	Fraction A(5,10);
 	cout << "Введите простую дробь: ";	cin >> A;
@@ -337,4 +363,38 @@ void main()
 	cout << "Введите три простые дроби: "; cin >> A >> B >> C;
 	cout << A << "\t" << B << "\t" << C << endl;
 #endif;
+#ifdef TYPE_CONVERSIONS
+	int a = 2;			//No conversions
+	double b = 3;		//Conversion from less to more (from int to double)
+	int c = b;			//Conversion from more to less wihout data loss (from double to int)
+	int d = 5.7;		//Conversion from more to less wih data loss (from double to int)
+	//int e = "Hello";	//Types not compatible
+	cout << 7. / 2 << endl; //Implicit conversion from less to more
+#endif;
+#ifdef CONVERSIONS_FROM_OTHER_TO_CLASS
+	Fraction A = (Fraction)5; //Conversion from 'int' to 'Fraction'
+					//Conversion from other to Class
+					//Single-arguement constructor
+	cout << A << endl;
+	cout << delimiter;
+	Fraction B;		//Default constructor
+	cout << delimiter;
+	B = Fraction(8);
+	cout << delimiter;
+	cout << B << endl;
+	//Fraction C = 12;	// explicit constructor невозможно вызвать оператором присваивания
+	//Fraction C(12);		// explicit constructor можно вызвать только так (есть ещё один способ)
+	Fraction C{ 12 };	//или так)
+#endif;
+#ifdef CONVERSIONS_FROM_CLASS_TO_OTHER
+	Fraction A(2, 3, 4);
+	cout << A << endl;
+	int a = (int)A;
+	cout << a <<endl;
+	double b = (double)A;
+	cout << b << endl;
+#endif;
+	Fraction A = 3.334;
+	cout << A << endl;
+
 }

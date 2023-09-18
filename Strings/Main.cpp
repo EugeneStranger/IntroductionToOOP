a78d290dcb3792aee6bff4376738bdcfd288d566
EugeneStranger <Stranger_1254@mail.ru>
2023-09-18 10:00:58 +0300

    String

--- Strings/Main.cpp
+++ Strings/Main.cpp
@@ -3,3 +3,119 @@ using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
+
+#define tab "\t"
+#define delimiter "--------------------------------------------------------------------\n"
+
+class String
+{
+	int size; //Размер строки лучше указывать в байтах
+	char* str; //Адрес строки в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	//			Constructors
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstructor:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; str[i]; i++)this->str[i] = str[i];
+	}
+	String(const String& other)
+	{
+		//Deep copy(побитовое копирование)
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] str;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	// operators
+	String& operator=(const String& other)
+	{
+		if (this == &other) return *this;
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:\t" << this << endl;
+		return *this;
+
+	}
+	char operator[](int i)const
+	{
+		return this->str[i];
+	}
+	char& operator[](int i)
+	{
+		return this->str[i];
+	}
+	//			Methods
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+
+	//
+
+};
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+String operator+(const String& left, const String& right)
+{
+	String cat(left.get_size()+right.get_size()-1);
+	for (int i = 0; i<left.get_size(); i++)
+		cat[i] = left[i];
+		//cat.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		cat[i+left.get_size()-1] = right[i];
+	return cat;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	cout << sizeof("Hello") << endl;
+	String str;
+	str.print();
+
+	String str1 = "Hello";
+	str1 = str1;
+	cout << str1 << endl;
+	String str2 = "World";
+	cout << str2 << endl;
+	cout << delimiter;
+	String str3 = str1+ str2;
+	cout << str3 << endl;
+
+	String str4;
+	str4 = str1 +" "+ str2;
+	cout << str4 << endl;
+
+
+}
